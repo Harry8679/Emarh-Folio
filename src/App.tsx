fa2ab0e3e075861f88f8f86a6e4132039d9ef074
Harry7986 <emarh.harry.code@gmail.com>
2026-01-08 01:33:35 +0100

    Portfolio 4

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ServicesPage from './pages/ServicesPage';
-import PortfolioPage from './pages/PortfolioPage';
-import ExperiencePage from './pages/ExperiencePage';
-import ContentPage from './pages/ContentPage';
-import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
